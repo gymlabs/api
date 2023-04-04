f0c1a2e8df3bd65158080076ed389bc9531abd1c
caldotdev <caldotdev@proton.me>
2023-04-04 16:23:55 +0200

    build: :heavy_plus_sign: add swc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import * as http from 'http';
+import http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!');
+  res.end('Hello, SWC!');
 });
 
 const port = process.env.PORT || 3000;
