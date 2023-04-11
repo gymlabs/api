f65791791405106602c317cc73cb964a74be26e2
caldotdev <caldotdev@proton.me>
2023-04-11 18:35:03 +0200

    build: :construction_worker: add tsup till I figure out how to do it with swc

---
+++ codegen/tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["./codegen/gql/graphql.ts"],
+  target: "es2022",
+  sourcemap: true,
+  clean: true,
+  dts: true,
+  format: ["cjs", "esm"],
+  outDir: "./codegen/dist",
+  minify: true,
+  bundle: true,
+});
