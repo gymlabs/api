49bc245e21d9e54324b0617beeb51f9ebe7d7726
Nicolas Kurzweil <nicolas_kurzweil@web.de>
2023-05-08 22:11:11 +0200

    feat: :sparkles: logout, delete user, change mail

---
+++ packages/gql.client/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @gymlabs/core.gql.client
+
+## 0.1.0
+
+### Minor Changes
+
+- logout, change mail, delete user
--- packages/server/src/schema/user/mutations.ts
+++ packages/server/src/schema/user/mutations.ts
@@ -3,6 +3,8 @@ import { ZodError } from "zod";
 
 import {
   AccessTokenResponse,
+  ChangeMailTokenAlreadyUsedError,
+  ChangeMailTokenExpiredError,
   EmailAlreadyInUseError,
   InvalidCredentialsError,
   InvalidEmailVerificationTokenError,
@@ -12,8 +14,7 @@ import {
 } from "./types";
 import { config } from "../../config";
 import { db } from "../../db";
-import { ForbiddenError, NotFoundError } from "../../errors";
-import { mapNullToUndefined } from "../../lib/mapNullToUndefined";
+import { NotFoundError } from "../../errors";
 import {
   comparePassword,
   hashPassword,
@@ -22,6 +23,7 @@ import {
 } from "../../lib/security";
 import { sendMail } from "../../services/mail/sendMail";
 import { EmailUpdatedEmail } from "../../services/mail/templates/EmailUpdatedEmail";
+import { ReactivationEmail } from "../../services/mail/templates/ReactivationEmail";
 import { ResetPasswordRequestEmail } from "../../services/mail/templates/ResetPasswordRequestEmail";
 import { WelcomeEmail } from "../../services/mail/templates/WelcomeEmail";
 import { builder } from "../builder";
@@ -115,88 +117,88 @@ builder.mutationFields((t) => ({
     },
   }),
 
-  updateUser: t.withAuth({ authenticated: true }).prismaFieldWithInput({
-    type: "User",
-    errors: { types: [ZodError] },
-    input: {
-      id: t.input.id(),
-      firstName: t.input.string({ required: false }),
-      lastName: t.input.string({ required: false }),
-      email: t.input.string({
-        required: false,
-        validate: {
-          email: true,
-        },
-      }),
-      password: t.input.string({
-        required: false,
-        validate: {
-          minLength: 10,
-          maxLength: 128,
-        },
-      }),
-    },
-    resolve: async (query, parent, { input: { id, ...input } }, context) => {
-      const user = await db.user.findUnique({
-        where: { id: context.viewer.user.id },
-      });
+  // updateUser: t.withAuth({ authenticated: true }).prismaFieldWithInput({
+  //   type: "User",
+  //   errors: { types: [ZodError] },
+  //   input: {
+  //     id: t.input.id(),
+  //     firstName: t.input.string({ required: false }),
+  //     lastName: t.input.string({ required: false }),
+  //     email: t.input.string({
+  //       required: false,
+  //       validate: {
+  //         email: true,
+  //       },
+  //     }),
+  //     password: t.input.string({
+  //       required: false,
+  //       validate: {
+  //         minLength: 10,
+  //         maxLength: 128,
+  //       },
+  //     }),
+  //   },
+  //   resolve: async (query, parent, { input: { id, ...input } }, context) => {
+  //     const user = await db.user.findUnique({
+  //       where: { id: context.viewer.user.id },
+  //     });
 
-      if (!user) {
-        throw new NotFoundError("User");
-      }
+  //     if (!user) {
+  //       throw new NotFoundError("User");
+  //     }
 
-      if (context.viewer.user.id !== id) {
-        throw new ForbiddenError("Cannot update another user.");
-      }
+  //     if (context.viewer.user.id !== id) {
+  //       throw new ForbiddenError("Cannot update another user.");
+  //     }
 
-      let password = input.password;
-      if (password) {
-        password = await hashPassword(password);
-      }
+  //     let password = input.password;
+  //     if (password) {
+  //       password = await hashPassword(password);
+  //     }
 
-      let emailData:
-        | {
-            emailVerificationToken: string;
-            isEmailVerified: true;
-          }
-        | undefined = undefined;
+  //     let emailData:
+  //       | {
+  //           emailVerificationToken: string;
+  //           isEmailVerified: true;
+  //         }
+  //       | undefined = undefined;
 
-      if (input.email) {
-        const emailVerificationToken = randomToken();
-        const emailVerificationTokenHash = await hashToken(
-          emailVerificationToken
-        );
-        emailData = {
-          emailVerificationToken: emailVerificationTokenHash,
-          isEmailVerified: true,
-        };
-      }
+  //     if (input.email) {
+  //       const emailVerificationToken = randomToken();
+  //       const emailVerificationTokenHash = await hashToken(
+  //         emailVerificationToken
+  //       );
+  //       emailData = {
+  //         emailVerificationToken: emailVerificationTokenHash,
+  //         isEmailVerified: true,
+  //       };
+  //     }
 
-      const updatedUser = await db.user.update({
-        ...query,
-        where: { id: String(id) },
-        data: mapNullToUndefined({
-          ...input,
-          password,
-          ...emailData,
-        }),
-      });
+  //     const updatedUser = await db.user.update({
+  //       ...query,
+  //       where: { id: String(id) },
+  //       data: mapNullToUndefined({
+  //         ...input,
+  //         password,
+  //         ...emailData,
+  //       }),
+  //     });
 
-      if (emailData) {
-        await sendMail(
-          new EmailUpdatedEmail(
-            updatedUser.firstName,
-            emailData.emailVerificationToken
-          ),
-          {
-            to: updatedUser.email,
-          }
-        );
-      }
+  //     if (emailData) {
+  //       await sendMail(
+  //         new EmailUpdatedEmail(
+  //           updatedUser.firstName,
+  //           emailData.emailVerificationToken
+  //         ),
+  //         {
+  //           to: updatedUser.email,
+  //         }
+  //       );
+  //     }
 
-      return updatedUser;
-    },
-  }),
+  //     return updatedUser;
+  //   },
+  // }),
 
   verifyEmail: t.fieldWithInput({
     type: "Boolean",
@@ -231,7 +233,6 @@ builder.mutationFields((t) => ({
 
   logout: t.withAuth({ authenticated: true }).field({
     type: "Boolean",
-    errors: { types: [] },
     description: "Invalidate the current access token.",
     args: {
       all: t.arg.boolean({
@@ -351,6 +352,134 @@ builder.mutationFields((t) => ({
       //sign out all
       await db.accessToken.deleteMany({
         where: { userId: resetPasswordRequest.userId },
+      });
+
+      return true;
+    },
+  }),
+
+  deleteAccount: t.withAuth({ authenticated: true }).field({
+    type: "Boolean",
+    description: "Delete the current user account.",
+    resolve: async (parent, { input }, context) => {
+      const token = randomToken();
+      const tokenHash = hashToken(token);
+      // delete after 30 days
+      const deleteAt: Date = new Date(Date.now() + 1000 * 60 * 60 * 24 * 30);
+
+      const user = await db.user.update({
+        where: { id: context.viewer.user.id },
+        data: {
+          deletedAt: deleteAt,
+          reactivationToken: tokenHash,
+        },
+      });
+
+      await sendMail(new ReactivationEmail(user.firstName, token, deleteAt), {
+        to: user.email,
+      });
+
+      return true;
+    },
+  }),
+
+  requestChangeMail: t.fieldWithInput({
+    type: "Boolean",
+    errors: { types: [ZodError] },
+    input: {
+      email: t.input.string({
+        validate: {
+          email: true,
+        },
+      }),
+      newMail: t.input.string({
+        validate: {
+          email: true,
+        },
+      }),
+    },
+    resolve: async (parent, { input }) => {
+      const user = await db.user.findUnique({
+        where: { email: input.email },
+      });
+
+      const newMailUser = await db.user.findUnique({
+        where: { email: input.newMail },
+      });
+
+      if (!user) throw new NotFoundError("User not found");
+      if (newMailUser)
+        throw new EmailAlreadyInUseError("New Email already in use");
+
+      const token = randomToken();
+      const tokenHash = hashToken(token);
+
+      const expiresAt = addMilliseconds(
+        new Date(),
+        config.security.changeMailRequestLifetime
+      );
+
+      await db.changeMailRequest.create({
+        data: {
+          userId: user.id,
+          token: tokenHash,
+          newMail: input.newMail,
+          expiresAt,
+        },
+      });
+
+      await sendMail(new EmailUpdatedEmail(user.firstName, token), {
+        to: user.email,
+      });
+
+      return true;
+    },
+  }),
+
+  changeMail: t.fieldWithInput({
+    type: "Boolean",
+    errors: {
+      types: [
+        ZodError,
+        InvalidEmailVerificationTokenError,
+        ChangeMailTokenAlreadyUsedError,
+        ChangeMailTokenExpiredError,
+      ],
+    },
+    input: { token: t.input.string() },
+    resolve: async (parent, { input }, context) => {
+      const changeMailRequest = await db.changeMailRequest.findUnique({
+        where: {
+          token: hashToken(input.token),
+        },
+      });
+
+      if (!changeMailRequest) {
+        throw new InvalidEmailVerificationTokenError();
+      }
+
+      if (changeMailRequest.usedAt) {
+        throw new ChangeMailTokenAlreadyUsedError();
+      }
+
+      const now = new Date();
+      if (changeMailRequest.expiresAt < now) {
+        throw new ChangeMailTokenExpiredError();
+      }
+
+      await db.user.update({
+        where: { id: changeMailRequest.userId },
+        data: { email: changeMailRequest.newMail },
+      });
+
+      await db.passwordResetRequest.update({
+        where: { id: changeMailRequest.id },
+        data: { usedAt: new Date() },
+      });
+
+      //sign out all
+      await db.accessToken.deleteMany({
+        where: { userId: changeMailRequest.userId },
       });
 
       return true;
