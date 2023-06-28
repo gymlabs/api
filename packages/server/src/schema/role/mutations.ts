1893a4b8b7889647ad324f339b53d32cc2ce91ad
Constantin <cluckenbach@protonmail.com>
2023-06-28 19:14:48 +0200

    feat: add update role mutation

--- packages/server/src/schema/role/mutations.ts
+++ packages/server/src/schema/role/mutations.ts
@@ -78,4 +78,68 @@ builder.mutationFields((t) => ({
       }
     },
   }),
+  updateRole: t.fieldWithInput({
+    type: Role,
+    input: {
+      id: t.input.string(),
+      name: t.input.string(),
+      accessRightIds: t.input.field({
+        type: ["String"],
+      }),
+    },
+    errors: {
+      types: [
+        InvalidArgumentError,
+        InternalServerError,
+        NotFoundError,
+        UnauthenticatedError,
+        UnauthorizedError,
+        ZodError,
+      ],
+    },
+    resolve: async (query, { input }, ctx) => {
+      if (!ctx.viewer.isAuthenticated()) throw new UnauthenticatedError();
+      try {
+        const role: Role__Output = await new Promise((resolve, reject) => {
+          client.updateRole(input, (err, res) => {
+            if (err) {
+              reject(err);
+            } else if (res) {
+              resolve(res);
+            }
+          });
+        });
+
+        const { accessRights, ...rest } = role;
+
+        const roleAccessRights =
+          accessRights?.accessRights.map((accessRight) => {
+            const { category, ...rest } = accessRight;
+            return {
+              ...rest,
+              category: category.toString() as keyof typeof Category,
+            };
+          }) || [];
+
+        return {
+          ...rest,
+          accessRights: roleAccessRights,
+          createdAt: new Date(role.createdAt),
+          updatedAt: new Date(role.updatedAt),
+        };
+      } catch (err) {
+        const error = err as grpc.ServiceError;
+        switch (error.code) {
+          case grpc.status.INVALID_ARGUMENT:
+            throw new InvalidArgumentError(error.message);
+          case grpc.status.NOT_FOUND:
+            throw new NotFoundError(error.message);
+          case grpc.status.PERMISSION_DENIED:
+            throw new UnauthorizedError();
+          default:
+            throw new InternalServerError();
+        }
+      }
+    },
+  }),
 }));
