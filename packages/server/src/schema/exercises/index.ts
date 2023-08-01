ecaa0fe410debde401cb1f1d7ea03eda08ee74d6
caldotdev <cluckenbach@protonmail.com>
2023-08-01 19:45:33 +0200

    refactor: :recycle: separate exercise queries & mutations

--- packages/server/src/schema/exercises/index.ts
+++ packages/server/src/schema/exercises/index.ts
@@ -1,3 +1,13 @@
-import "./mutations";
-import "./queries";
+// queries
+import "./queries/many";
+import "./queries/one";
+
+// mutations
+import "./mutations/create";
+import "./mutations/createStep";
+import "./mutations/delete";
+import "./mutations/deleteStep";
+import "./mutations/update";
+import "./mutations/updateStep";
+
 import "./types";
---
+++ packages/server/src/schema/exercises/mutations/delete.ts
@@ -0,0 +1,79 @@
+import { ZodError, z } from "zod";
+
+import { db } from "../../../db";
+import {
+  InvalidArgumentError,
+  NotFoundError,
+  InternalServerError,
+  UnauthenticatedError,
+  UnauthorizedError,
+} from "../../../errors";
+import validationWrapper from "../../../errors/validationWrapper";
+import { authenticateOrganizationEntity } from "../../../lib/authenticate";
+import { builder } from "../../builder";
+
+builder.mutationField("deleteExercise", (t) =>
+  t.fieldWithInput({
+    type: "Boolean",
+    input: {
+      id: t.input.string(),
+    },
+    errors: {
+      types: [
+        ZodError,
+        InvalidArgumentError,
+        NotFoundError,
+        InternalServerError,
+        UnauthenticatedError,
+        UnauthorizedError,
+      ],
+    },
+    resolve: async (query, { input }, ctx) => {
+      if (!ctx.viewer.isAuthenticated()) {
+        throw new UnauthenticatedError();
+      }
+
+      const wrapped = async () => {
+        const exercise = await db.exercise.findUnique({
+          where: {
+            id: input.id,
+          },
+        });
+
+        if (!exercise) {
+          throw new NotFoundError("Exercise");
+        }
+
+        if (
+          !(await authenticateOrganizationEntity(
+            "EXERCISE",
+            "delete",
+            ctx.viewer.user?.id ?? "",
+            exercise.organizationId
+            /* TODO: eventuell die orga ids und gym ids in den viewer
+            würde das alles etwas cleaner machen (auch an anderen Stellen)
+            */
+          ))
+        ) {
+          throw new UnauthorizedError();
+        }
+
+        await db.exercise.delete({
+          where: {
+            id: input.id,
+          },
+        });
+
+        return true;
+      };
+
+      return await validationWrapper(
+        wrapped,
+        z.object({
+          id: z.string().uuid(),
+        }),
+        input
+      );
+    },
+  })
+);
