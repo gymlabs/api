4bff47b95abf50fd4b16b2ae28ea7e83a5806e3b
caldotdev <cluckenbach@protonmail.com>
2023-08-05 11:16:14 +0200

    fix: :poop: fix tsx build error

--- packages/client/web/src/pregen.ts
+++ packages/client/web/src/pregen.ts
@@ -2,7 +2,10 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 import { printSchema } from "graphql";
 
 // TODO: fix this import 🤮
-import { schema } from "../../server/src/schema";
+// import { schema } from "../../server/src/schema";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { schema } from "../../../server/build/schema";
 
 const config: CodegenConfig = {
   schema: printSchema(schema),
